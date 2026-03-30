c20197efb97af18651bc108c231c3d45d465107c
fetijashari <fetijashari@gmail.com>
2026-03-30 18:40:00 +0200

    Refactor BusinessCentral module to include KNOWN_BC_ENTITIES constant

--- lib/business_central.rb
+++ lib/business_central.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'logger'
 require 'oauth2'
 require 'oauth2/error'
 require 'net/http'
@@ -22,3 +23,20 @@ require 'business_central/object/attachments'
 require 'business_central/exceptions'
 require 'business_central/client'
 require 'business_central/web_service'
+
+module BusinessCentral
+  KNOWN_BC_ENTITIES = %w[
+    vendors customers items companies
+    sales_invoices sales_orders sales_credit_memos sales_quotes
+    purchase_invoices purchase_orders
+    general_ledger_entries
+    accounts journals journal_lines
+    employees dimensions default_dimensions
+    currencies countries_regions payment_terms payment_methods
+    shipment_methods item_categories
+    tax_groups tax_areas
+    units_of_measure
+    picture attachments
+    company_information
+  ].freeze
+end
