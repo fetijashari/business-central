1f5b3f1f28ec6bd1c98162c9b6095e4f246db894
fetijashari <fetijashari@gmail.com>
2026-03-30 16:04:57 +0200

    Refactor Client class to improve credential and URL assignment methods for better readability and maintainability

--- lib/business_central/client.rb
+++ lib/business_central/client.rb
@@ -6,9 +6,9 @@ module BusinessCentral
 
     include BusinessCentral::Object::ObjectHelper
 
-    DEFAULT_LOGIN_URL = 'https://login.microsoftonline.com/common'.freeze
+    DEFAULT_LOGIN_URL = 'https://login.microsoftonline.com/common'
 
-    DEFAULT_URL = 'https://api.businesscentral.dynamics.com/v2.0/production/api/v1.0'.freeze
+    DEFAULT_URL = 'https://api.businesscentral.dynamics.com/v2.0/production/api/v1.0'
 
     DEFAULT_OPEN_TIMEOUT = 30
     DEFAULT_READ_TIMEOUT = 60
@@ -31,19 +31,9 @@ module BusinessCentral
 
     def initialize(options = {})
       opts = options.dup
-      @username = opts.delete(:username)
-      @password = opts.delete(:password)
-      @url = opts.delete(:url) || DEFAULT_URL
-      @web_service_url = opts.delete(:web_service_url)
-      @application_id = opts.delete(:application_id)
-      @secret_key = opts.delete(:secret_key)
-      @oauth2_login_url = opts.delete(:oauth2_login_url) || DEFAULT_LOGIN_URL
-      @default_company_id = opts.delete(:default_company_id)
-      @debug = opts.delete(:debug) || false
-      @debug_output = opts.delete(:debug_output) || $stdout
-      @open_timeout = opts.delete(:open_timeout) || DEFAULT_OPEN_TIMEOUT
-      @read_timeout = opts.delete(:read_timeout) || DEFAULT_READ_TIMEOUT
-
+      assign_credentials(opts)
+      assign_urls(opts)
+      assign_options(opts)
       validate_urls!
     end
 
@@ -66,9 +56,9 @@ module BusinessCentral
       @oauth2_access_token = OAuth2::AccessToken.new(
         oauth2_client,
         token,
-        refresh_token: refresh_token,
-        expires_at: expires_at,
-        expires_in: expires_in
+        refresh_token:,
+        expires_at:,
+        expires_in:
       )
     end
 
@@ -83,6 +73,27 @@ module BusinessCentral
     end
 
     private
+
+    def assign_credentials(opts)
+      @username = opts.delete(:username)
+      @password = opts.delete(:password)
+      @application_id = opts.delete(:application_id)
+      @secret_key = opts.delete(:secret_key)
+    end
+
+    def assign_urls(opts)
+      @url = opts.delete(:url) || DEFAULT_URL
+      @web_service_url = opts.delete(:web_service_url)
+      @oauth2_login_url = opts.delete(:oauth2_login_url) || DEFAULT_LOGIN_URL
+    end
+
+    def assign_options(opts)
+      @default_company_id = opts.delete(:default_company_id)
+      @debug = opts.delete(:debug) || false
+      @debug_output = opts.delete(:debug_output) || $stdout
+      @open_timeout = opts.delete(:open_timeout) || DEFAULT_OPEN_TIMEOUT
+      @read_timeout = opts.delete(:read_timeout) || DEFAULT_READ_TIMEOUT
+    end
 
     def oauth2_client
       @oauth2_client ||= OAuth2::Client.new(
@@ -101,7 +112,7 @@ module BusinessCentral
       when 'invalid_grant'
         raise InvalidGrantException, error.message
       end
-      raise ApiException.new(error.message)
+      raise ApiException, error.message
     end
 
     def validate_urls!
@@ -116,7 +127,8 @@ module BusinessCentral
       uri = URI.parse(url)
       return if uri.is_a?(URI::HTTPS)
 
-      raise ArgumentError, "#{name} must use HTTPS scheme, got: #{uri.scheme || 'none'}"
+      raise ArgumentError,
+            "#{name} must use HTTPS scheme, got: #{uri.scheme || 'none'}"
     rescue URI::InvalidURIError => e
       raise ArgumentError, "#{name} is not a valid URL: #{e.message}"
     end
